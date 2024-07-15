1fe3a72701daddcfedbda405d1380cf154dd3e9c
Danniel Navas <dannielnavas@gmail.com>
2024-07-15 07:56:09 -0500

    ✨ chore: Add lazy loading for images and update Redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,47 @@
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import { getPokemon } from "./api/index";
 import "./App.css";
 import { PokemonList } from "./components/PokemonList";
 import { Search } from "./components/Search";
+import { setPokemons as setPokemonsAction } from "./store/actions/index";
 
-function App() {
+function App({ pokemons, setPokemons }) {
+  // const [pokemons, setPokemons] = useState([]);
+  useEffect(() => {
+    try {
+      const fetchPokemon = async () => {
+        const data = await getPokemon();
+        setPokemons(data);
+      };
+      fetchPokemon();
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  console.log(pokemons);
   return (
     <div className="container">
       <Search />
       <PokemonList
-        pokemons={[
-          {
-            id: 0,
-            name: "Bulbasaur",
-            image:
-              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
-            type: "Grass",
-          },
-          {
-            id: 1,
-            name: "Ivysaur",
-            image:
-              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
-            type: "Grass",
-          },
-          {
-            id: 2,
-            name: "Venusaur",
-            image:
-              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
-            type: "Grass",
-          },
-          {
-            id: 3,
-            name: "Charmander",
-            image:
-              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
-            type: "Fire",
-          },
-          {
-            id: 4,
-            name: "Charmeleon",
-            image:
-              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png",
-            type: "Fire",
-          },
-          {
-            id: 5,
-            name: "Charizard",
-            image:
-              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
-            type: "Fire",
-          },
-          {
-            id: 6,
-            name: "Squirtle",
-            image:
-              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
-            type: "Water",
-          },
-          {
-            id: 7,
-            name: "Wartortle",
-            image:
-              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png",
-            type: "Water",
-          },
-          {
-            id: 8,
-            name: "Blastoise",
-            image:
-              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png",
-            type: "Water",
-          },
-        ]}
+        pokemons={pokemons.map((pokemon: any, index: number) => ({
+          id: index,
+          name: pokemon.name,
+          image: `https://pokeres.bastionbot.org/images/pokemon/${index + 1}.png`,
+          type: pokemon.type,
+        }))}
       />
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  pokemons: state.pokemons,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setPokemons: (value) => dispatch(setPokemonsAction(value)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
